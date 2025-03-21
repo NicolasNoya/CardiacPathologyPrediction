48e6892abca9474e2eab401927c54892210b0934
nnoya <nnoya@fing.edu.uy>
2025-03-21 10:45:56 +0100

    fix small error with the packaging

--- research/21032025_training_unet.py
+++ research/21032025_training_unet.py
@@ -1,21 +1,17 @@
-# MODEL HUB: https://huggingface.co/amal90888/unet-segmentation-model
-#%%
 import torch
 import tensorflow as tf
 import os
 # import niidataloader
 import sys
-sys.path.append('../niidataloader')
-from niidataloader import NiftiDataset
+# sys.path.append('../niidataloader')
+from ..niidataloader import NiftiDataset
 from huggingface_hub import hf_hub_download
 from tensorflow.keras.models import load_model
 from keras.saving import register_keras_serializable
 import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
 from torch.nn import functional as F
-#%%
 
-# ✅ Set Keras backend (optional)
 os.environ["KERAS_BACKEND"] = "jax"
 
 # ✅ Register and define missing functions
@@ -42,10 +38,6 @@ unet.compile(optimizer=Adam(learning_rate=1e-4), loss="binary_crossentropy", met
 
 print("✅ Model loaded and recompiled successfully!")
 
-#%%
-unet.summary()
-
-#%%
 # Dice loss
 def dice_loss(y_true, y_pred, smooth=1e-6):
     y_true_f = K.flatten(y_true)
@@ -54,75 +46,17 @@ def dice_loss(y_true, y_pred, smooth=1e-6):
     return 1 - (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 
-
-#%%
 # freeze the model
 for layer in unet.layers:
     layer.trainable = False
 
 # unfreeze the lasts layers
-unet.layers[-1].trainable = True
-unet.layers[-2].trainable = True
-unet.layers[-3].trainable = True
-unet.layers[-4].trainable = True
-unet.layers[-5].trainable = True
-unet.layers[-6].trainable = True
-unet.layers[-7].trainable = True
-unet.layers[-8].trainable = True
-unet.layers[-9].trainable = True
-unet.layers[-10].trainable = True
-unet.layers[-11].trainable = True
-unet.layers[-12].trainable = True
-unet.layers[-13].trainable = True
-unet.layers[-14].trainable = True
-unet.layers[-15].trainable = True
-unet.layers[-16].trainable = True
-unet.layers[-17].trainable = True
-unet.layers[-18].trainable = True
-unet.layers[-19].trainable = True
-unet.layers[-20].trainable = True
-unet.layers[-21].trainable = True
-unet.layers[-22].trainable = True
-unet.layers[-23].trainable = True
-# unet.layers[-24].trainable = True
-# unet.layers[-25].trainable = True
-# unet.layers[-26].trainable = True
+for i in range(1, 24):
+    unet.layers[-i].trainable = True
 
-unet.summary()
 
-#%%
 data = NiftiDataset("./data/Train")
 
-#%%
-print(data[0][0,:,:,0].shape)
-plt.imshow(data[0][0,:,:,0], cmap="gray")
-plt.show()
-plt.imshow(data[0][1,:,:,0], cmap="gray")
-plt.show()
-#%%
-# Resize the image
-print(data[0][0,:,:,0].shape)
-image = data[0][0,:,:,0].reshape(1, 1, 220, 220)
-print(image.shape)
-resized_image = F.interpolate(image, size=(128, 128), mode='bilinear', align_corners=False).reshape(128,128)
-print(resized_image.shape)
-
-#%%
-# Pass the image trough the model
-predicted_mask = unet.predict(resized_image.reshape(1, 128, 128, 1))
-print(predicted_mask.shape)
-print("PREDICTED")
-plt.imshow(predicted_mask[0,:,:,0], cmap="gray")
-plt.show()
-print("ORIGINAL")
-plt.imshow(data[0][1,:,:,0], cmap="gray")
-plt.show()
-
-#%%
-print(data[32])
-#%%
-# Train the model Toy example
-# Prepare the data
 images = [F.interpolate(data[i][0,:,:,0].reshape(1,1,220,220), size=(128,128), 
                         mode='bilinear', align_corners=False).reshape(1,128,128)
            for i in range(32)]
@@ -149,20 +83,3 @@ unet.compile(optimizer=Adam(learning_rate=1e-4), loss=dice_loss, metrics=["accur
 # Train the model
 unet.fit(train_dataset, epochs=1000)
 
-#%%
-print(images.shape)
-plt.imshow(images[0], cmap="gray")
-plt.show()
-#%%
-# inference
-predicted_mask = unet.predict(images)
-#%%
-print(predicted_mask.shape)
-#%%
-plt.imshow(predicted_mask[0,:,:,0], cmap="gray")
-plt.show()
-# plt.imshow(images[0], cmap="gray")
-#%%
-print(masks.shape)
-plt.imshow(masks[0,:,:], cmap="gray")
-plt.show()
